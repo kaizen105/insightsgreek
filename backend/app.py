63577e9b99858234c4b21a18647c78f8ba548357
Kaizen105 <yasharma490@gmail.com>
2025-11-19 18:20:50 +0530

    hh

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ else:
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
-CODE_DIR = os.path.join(PROJECT_ROOT, 'code')
+CODE_DIR = os.path.join(PROJECT_ROOT, 'code_ml')
 CURRENT_DIR = BASE_DIR
 
 if PROJECT_ROOT not in sys.path: 
